d434ce06069d299ed2c8fa8478eb7d8ea6f71adf
Landon Thull <landonthull.tx@gmail.com>
2023-09-15 16:03:58 -0500

    add 0070 - climbing stairs

---
+++ cpp/0070-climbing-stairs/0070-fibbonaci-sequence.cpp
@@ -0,0 +1,41 @@
+// source : https://leetcode.com/problems/climbing-stairs
+// author : Landon Thull
+// date   : 09-15-2023
+
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+Constraints:
+1 <= n <= 45
+*/
+
+class Solution {
+public:
+  int climbStairs(int n) {
+    int first = 1, second = 1;
+
+    for (int i = 0; i < n - 1; i++) {
+      int temp = first;
+      first += second;
+      second = temp;
+    }
+    
+    return first;
+  }
+};
