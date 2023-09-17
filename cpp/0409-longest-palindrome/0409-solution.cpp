e3ee89da7ca941ef68ab04ca51b711ca43d04e09
Landon Thull <landonthull.tx@gmail.com>
2023-09-16 23:33:13 -0500

    add 0409 - longest palindrome

---
+++ cpp/0409-longest-palindrome/0409-solution.cpp
@@ -0,0 +1,49 @@
+// source : https://leetcode.com/problems/longest-palindrome
+// author : Landon Thull
+// date   : 09-16-2023
+
+/*
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+Example 1:
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+Example 2:
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is 1.
+
+Constraints:
+1 <= s.length <= 2000
+s consists of lowercase and/or uppercase English letters only.
+*/
+
+class Solution {
+public:
+  int longestPalindrome(string s) {
+    int l[128] = {0};
+
+    int res = 0;
+
+    for (const char& c : s) {
+      cout << c << endl;
+      l[c]++;
+      if (l[c] == 2) {
+        res += 2;
+        l[c] = 0;
+      }
+    }
+
+    for (const int& i : l) {
+      if (i != 0) {
+        res++;
+        break;
+      }
+    }
+
+    return res;
+  }
+};
