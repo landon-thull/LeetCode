830d3b9acb0b39c918e9c41099cbbc766b97040a
Landon Thull <landonthull.tx@gmail.com>
2023-09-30 23:42:41 -0500

    add 0057 - insert interval

---
+++ cpp/0057-insert-interval/0057-solution.cpp
@@ -0,0 +1,43 @@
+// source : https://leetcode.com/problems/insert-interval
+// author : Landon Thull
+// date   : 09-30-2023
+
+class Solution {
+public:
+    bool doIntervalsOverlap(vector<int>& a, vector<int>& b) {
+        return min(a[1], b[1]) - max(a[0], b[0]) >= 0;
+    }
+
+    vector<int> mergeIntervals(vector<int>& a, vector<int>& b) {
+        return {min(a[0], b[0]), max(a[1], b[1])};
+    }
+
+    void insertInterval(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int index = upper_bound(intervals.begin(), intervals.end(), newInterval) - intervals.begin();
+
+        if (index != intervals.size()) {
+            intervals.insert(intervals.begin() + index, newInterval);
+        } else {
+            intervals.push_back(newInterval);
+        }
+    }
+
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        insertInterval(intervals, newInterval);
+
+        vector<vector<int>> res;
+        for (int i = 0; i < intervals.size(); i++) {
+            vector<int> currInterval = {intervals[i][0], intervals[i][1]};
+
+            while (i < intervals.size() && doIntervalsOverlap(currInterval, intervals[i])) {
+                currInterval = mergeIntervals(currInterval, intervals[i]);
+                i++;
+            }
+
+            i--;
+            res.push_back(currInterval);
+        }
+
+        return res;
+    }
+};
