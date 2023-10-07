e9cb2d1a05de52feeeb1cfaaf2f6b32ad99f4f5e
Landon Thull <landonthull.tx@gmail.com>
2023-10-06 23:58:11 -0500

    add 0542 - 01 matrix (python)

---
+++ python/0542-01-matrix/0542-dp.py
@@ -0,0 +1,32 @@
+# source : https://leetcode.com/problems/01-matrix
+# author : Landon Thull
+# date   : 10-06-2023
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        dp = [row[:] for row in mat]
+        m, n = len(dp), len(dp[0])
+
+        for row in range(m):
+            for col in range(n):
+                min_neighbor = inf
+                if dp[row][col] != 0:
+                    if row > 0:
+                        min_neighbor = min(min_neighbor, dp[row - 1][col])
+                    if col > 0:
+                        min_neighbor = min(min_neighbor, dp[row][col - 1])
+                        
+                    dp[row][col] = min_neighbor + 1
+    
+        for row in range(m - 1, -1, -1):
+            for col in range(n - 1, -1, -1):
+                min_neighbor = inf
+                if dp[row][col] != 0:
+                    if row < m - 1:
+                        min_neighbor = min(min_neighbor, dp[row + 1][col])
+                    if col < n - 1:
+                        min_neighbor = min(min_neighbor, dp[row][col + 1])
+                        
+                    dp[row][col] = min(dp[row][col], min_neighbor + 1)
+
+        return dp
